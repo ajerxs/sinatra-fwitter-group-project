b22374d30aa08323d2f7ac87930a38aa568b03c0
Andrew Eames <ajerxs@gmail.com>
2021-07-25 19:48:00 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,73 @@
 class TweetsController < ApplicationController
+ 
+    get '/tweets' do 
+        if logged_in?
+            @user = current_user
+            @tweets = Tweet.all
+            erb :'tweets/tweets'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do 
+        if !params[:content].empty?
+            @tweet = Tweet.create(content: params[:content])
+            current_user.tweets << @tweet
+            current_user.save
+            redirect '/tweets'
+        else
+            redirect to '/tweets/new'
+        end
+    end
+
+    get '/tweets/new' do
+        if logged_in?
+            erb :'tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id' do 
+        if logged_in?
+            @user = current_user
+            @tweet = Tweet.find_by(id: params[:id])
+            erb :'tweets/show_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        @tweet = Tweet.find_by(params[:id])
+        if logged_in? && @tweet.user == current_user
+            erb :'tweets/edit_tweet'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by(id: params[:id])
+        if !params[:content].empty?
+            @tweet.update(content: params[:content])
+            @tweet.save
+            redirect "/tweets/#{params[:id]}"
+        else
+            redirect "/tweets/#{params[:id]}/edit"
+        end
+    end
+
+    post '/tweets/:id/delete' do
+        @tweet = Tweet.find_by(id: params[:id])
+        if @tweet.user == current_user
+            @tweet.delete
+            redirect to '/tweets'
+        else
+            redirect to "/tweets/#{params[:id]}"
+        end
+    end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    name = self.username
+    @slug = name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect do |name|
+      name.slug == slug
+    end
+  end
 end
